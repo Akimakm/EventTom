f1d3f794e144ceb0e2c307ba0ef3567b2f676c24
Loic Tsayem <melachioloic99@yahoo.com>
2024-12-09 15:01:23 +0100

    Initial commit

---
+++ src/main/java/com/eventtom/eventtom/EventtomApplication.java
@@ -0,0 +1,13 @@
+package com.eventtom.eventtom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventtomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventtomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventtom/eventtom/EventtomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventtom.eventtom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventtomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
