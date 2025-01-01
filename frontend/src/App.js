b9445c9a9cdcdcc620234d6d592b8ff784ea766c
Loic Tsayem <melachioloic99@yahoo.com>
2025-01-01 16:23:44 +0100

    2-home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./presentation/view/Login.tsx";
 import EventList from "./presentation/view/EventList.tsx"; // List of all events
 import EventDetailsWrapper from "./presentation/view/EventDetailsWrapper.tsx"; // Event details based on role
+import HomeEvent from "./presentation/view/HomeEvent";
+import HomeEventStrategy from "./presentation/strategy/HomeEventStrategy";
 import './App.css';
 
 function App() {
@@ -13,6 +14,10 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/events" element={<EventList />} />
           <Route path="/events/:id/details" element={<EventDetailsWrapper />} />
+          <Route path="/home" element={<HomeEvent />} />
+          <Route path="/home-event-strategy" element={<HomeEventStrategy />} />
+
+
         </Routes>
       </Router>
   );
